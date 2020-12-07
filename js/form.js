a3038b2a95216e82b1d78870089ced4c0c862e4f
Jagrit159 <71010470+Jagrit159@users.noreply.github.com>
2020-12-07 16:24:48 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,32 @@
+class Form{
+    constructor(){
+     this.input = createInput("name");
+     this.button = createButton("play");
+     this.greetings = createElement("h5");
+    }
+
+    hide(){
+     this.greetings.hide()
+     this.input.hide()
+     this.button.hide()
+    }
+
+display(){
+ var Title = createElement("h2")
+ Title.html("Car Racing")
+ Title.position(displayWidth/2-50,0)
+ this.input.position(displayWidth/2-40,displayHeight/2-80);
+ this.button.position(displayWidth/2+30,displayHeight/2);
+ this.button.mousePressed(()=>{
+ this.input.hide()
+ this.button.hide()
+ player.name = this.input.value()
+    playerCount = playerCount+1
+    player.index = playerCount;
+    player.update()
+    player.updateCount(playerCount)
+    this.greetings.html("hello!"+ player.name)
+    this.greetings.position(displayWidth/2-70,displayHeight/2);
+ })
+}
+};
